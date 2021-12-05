52a5c5a6ad378d32281f075e79a953a82699cb81
Paul  Hazell <36421746+CptnCrumble@users.noreply.github.com>
2021-12-05 10:53:32 +0000

    tidy up

--- testxlsxcode.js
+++
@@ -1,7 +0,0 @@
-if(typeof require !== 'undefined') XLSX = require('xlsx');
-
-var workbook = XLSX.readFile('PKGv2.xlsx');
-
-
-console.log(workbook.SheetNames)
-console.log(workbook.SheetNames.find( n => n === '69 Month Qs'))
